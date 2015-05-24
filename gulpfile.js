824fb6aef003a034817b482f6bf4620ab64993c6
Rplus <rplus.tw@gmail.com>
2015-05-24 17:30:05 +0800

    調整 browser-sync task

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,6 @@ gulp.task('css', ['clean:css'], function () {
       require('autoprefixer')({browsers: ['last 1 version']})
     ]))
     .pipe(gulp.dest(path.distDir + '/css/'))
-    .pipe($.filter('**/*.css'))
-    .pipe(reload({stream:true}))
 });
 
 gulp.task('js', function () {
@@ -32,7 +30,6 @@ gulp.task('js', function () {
     .pipe($.plumber())
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')))
     .pipe($.babel())
     .pipe($.uglify())
     .pipe($.rename(function (path) {
@@ -44,8 +41,6 @@ gulp.task('js', function () {
 gulp.task('html', function () {
   return gulp.src(path.srcDir + '/html/*.html')
     .pipe(gulp.dest(path.distDir + '/html/'))
-    .pipe($.filter('**/*.js'))
-    .pipe(reload({stream:true}))
 });
 
 gulp.task('clean', del.bind(null, [path.distDir]));
@@ -54,14 +49,15 @@ gulp.task('clean:css', del.bind(null, [path.distDir + '/css']));
 // Watch Files For Changes & Reload
 gulp.task('serve', ['default'], function () {
   browserSync({
+    browser: 'google-chrome',
     notify: false,
     server: path.distDir
   });
 
   // watch the folder to reload if files was change
-  gulp.watch([path.srcDir + '/js/*.js'], ['js']);
-  gulp.watch([path.srcDir + '/css/*.css'], ['css']);
-  gulp.watch([path.srcDir + '/html/*.html'], ['html'], reload);
+  gulp.watch([path.srcDir + '/js/*.js'], ['js', reload]);
+  gulp.watch([path.srcDir + '/css/*.{styl,scss}'], ['css', reload]);
+  gulp.watch([path.srcDir + '/html/*.html'], ['html', reload]);
 });
 
 gulp.task('default', [], function () {
