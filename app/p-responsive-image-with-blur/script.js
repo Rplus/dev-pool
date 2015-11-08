809678a92899fd7705e1a7bb04a61a247a05406d
Rplus <rplus.tw@gmail.com>
2015-11-09 02:00:08 +0800

    加入 scroll event, 以完成 lazy-loading

--- app/p-responsive-image-with-blur/script.js
+++ app/p-responsive-image-with-blur/script.js
@@ -1,6 +1,6 @@
 ;(function() {
 
-  let progressiveImages = document.querySelectorAll('.image-thumbnail');
+  let progressiveImages = [].map.call(document.querySelectorAll('.image-thumbnail'), (i) => i);
 
   let originImgLoaded = function(oimg) {
     oimg.parentNode.className += ' done ';
@@ -16,8 +16,35 @@
     thumbImg.parentNode.appendChild(originImg);
   };
 
-  [].forEach.call(progressiveImages, function(pimg) {
-    insertOriginImg(pimg);
+  let detectImgInViewport = function() {
+    // if no image waiting for calculating position, return
+    if (!progressiveImages.length) { return; }
+
+    let winH = window.innerHeight;
+    let _indexWaitingforRemove = [];
+
+    progressiveImages.forEach(function(pimg, i) {
+      let _rect = pimg.getBoundingClientRect();
+
+      if ((_rect.top > 0 && _rect.top < winH) || (_rect.bottom > 0 && _rect.bottom < winH)) {
+        let _index = progressiveImages.indexOf(pimg);
+        _indexWaitingforRemove.push(_index);
+        insertOriginImg(pimg);
+      }
+    });
+
+    // remove images from calculating position
+    if (_indexWaitingforRemove.length) {
+      progressiveImages.splice(_indexWaitingforRemove[0], _indexWaitingforRemove.length);
+    }
+  };
+
+  window.addEventListener('scroll', function() {
+    detectImgInViewport();
   });
+
+  let init = (function() {
+    detectImgInViewport();
+  })();
 
 })();
