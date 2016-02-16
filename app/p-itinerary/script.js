047e663f32aa4066e8c23e81669d7f765355090a
Rplus <rplus.tw@gmail.com>
2016-02-16 21:27:37 +0800

    add clone event for BTN: new day

--- app/p-itinerary/script.js
+++ app/p-itinerary/script.js
@@ -1,8 +1,7 @@
 let card = {};
 
 card.wrap = document.querySelector('.cards');
-card.items = [].slice.call(card.wrap.querySelectorAll('.card'));
-card.titles = [].slice.call(card.wrap.querySelectorAll('.card__title'));
+card.new = document.querySelector('.new-card');
 
 card.wrap.addEventListener('click', (e) => {
   if (e.target.classList.contains('card__title')) {
@@ -17,3 +16,23 @@ card.wrap.addEventListener('click', (e) => {
     card.wrap.classList.toggle('is-active', !isActive);
   }
 });
+
+card.new.addEventListener('click', () => {
+  let lastCard = card.wrap.querySelector('.card:last-child');
+  let lastNumber = +lastCard.className.match(/\d+/);
+  let newCard = lastCard.cloneNode(true);
+  newCard.className = newCard.className.replace(/\d+/, lastNumber + 1);
+
+  lastCard.parentElement.insertBefore(newCard, lastCard.nextSibling);
+
+  reCalcPos();
+});
+
+let reCalcPos = () => {
+  let cards = [].slice.call(card.wrap.querySelectorAll('.card'));
+  let count = cards.length;
+
+  cards.forEach((card, i) => {
+    card.style.top = `calc(${i / count} * (100% - 1.5rem))`;
+  });
+};
