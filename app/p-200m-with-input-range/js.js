4a6b6cb260b03720eab1869b90b0a5c0d3d81868
Rplus <rplus.tw@gmail.com>
2016-10-08 16:30:34 +0800

    refactor with ES2015 class

--- app/p-200m-with-input-range/js.js
+++ app/p-200m-with-input-range/js.js
@@ -1,53 +1,104 @@
 /* global document */
-var myLatLng = {};
 
-var getGeoFallback = () => {
-  var _lat = window.prompt('Can\'t find geo-location automatically, please fill with latitude(lat)', '25.0327429');
-  var _lng = window.prompt('Can\'t find geo-location automatically, please fill with longitude(lng)', '121.5668311');
+class GeoMap {
+  constructor () {
+    this.ele = {
+      mapBox: document.querySelector('.map'),
 
-  myLatLng.lat = parseFloat(_lat) || 25.0327429;
-  myLatLng.lng = parseFloat(_lng) || 121.5668311;
+      axisAX: document.querySelector('.axis-ax'),
+      axisAY: document.querySelector('.axis-ay'),
 
-  init();
-};
+      axisBX: document.querySelector('.axis-bx'),
+      axisBY: document.querySelector('.axis-by'),
 
-if ('geolocation' in navigator) {
-  navigator.geolocation.getCurrentPosition((geo) => {
-    myLatLng.lat = geo.coords.latitude;
-    myLatLng.lng = geo.coords.longitude;
+      pointA: document.querySelector('.point--a'),
+      pointB: document.querySelector('.point--b')
+    };
 
-    init();
-  }, getGeoFallback);
-} else {
-  getGeoFallback();
+    this.latLng = {};
+    this.mapZoom = 17;
+    this.mapSize = 600;
+    this.key = 'AIzaSyCjDnDGv67nvhzBsLRYAwTbiF1HrZBQDUM';
+  }
+
+  getGeoFallback () {
+    console.log(111111);
+
+    let _lat = window.prompt('Can\'t find geo-location automatically, please fill with latitude(lat)', '25.0327429');
+    let _lng = window.prompt('Can\'t find geo-location automatically, please fill with longitude(lng)', '121.5668311');
+
+    this.latLng.lat = parseFloat(_lat) || 25.0327429;
+    this.latLng.lng = parseFloat(_lng) || 121.5668311;
+
+    return this.latLng;
+  }
+
+  getGeo () {
+    let self = this;
+    return new Promise(function (resolve, reject) {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          resolve({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+        }, () => {
+          resolve(self.getGeoFallback());
+        });
+      } else {
+        resolve(this.getGeoFallback());
+      }
+    });
+  }
+
+  getGmapImgSrc (latLng = this.latLng, zoom = this.mapZoom, size = this.mapSize) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${latLng.lat},${latLng.lng}&zoom=${zoom}&size=${size}x${size}&markers=${latLng.lat},${latLng.lng}&key=${this.key}`;
+  }
+
+  setEleBgi (ele, bgiSrc) {
+    ele.style.backgroundImage = `url('${bgiSrc}')`;
+  }
+
+  updateBgi () {
+    this.getGeo()
+      .then((latLng) => {
+        this.setEleBgi(this.ele.mapBox, this.getGmapImgSrc(latLng));
+      });
+  }
+
+  bindMoveEvent () {
+    let eventMapping = [
+      {
+        axis: 'axisAX',
+        target: 'pointA'
+      },
+      {
+        axis: 'axisAY',
+        target: 'pointA'
+      },
+      {
+        axis: 'axisBX',
+        target: 'pointB'
+      },
+      {
+        axis: 'axisBY',
+        target: 'pointB'
+      }
+    ];
+
+    eventMapping.forEach((i) => {
+      let dir = (i.axis.slice(-1) === 'Y' ? 'top' : 'left');
+
+      this.ele[i.axis].addEventListener('input', (e) => {
+        this.ele[i.target].style[dir] = `${e.target.value}%`;
+      });
+    });
+  }
+
+  init () {
+    this.updateBgi();
+    this.bindMoveEvent();
+  }
 }
 
-var init = () => {
-  var mapBox = document.querySelector('.map');
-  mapBox.style.backgroundImage = `url(https://maps.googleapis.com/maps/api/staticmap?center=${myLatLng.lat},${myLatLng.lng}&zoom=17&size=600x600&markers=${myLatLng.lat},${myLatLng.lng})`;
-
-  var axisAX = document.querySelector('.axis-ax');
-  var axisAY = document.querySelector('.axis-ay');
-
-  var axisBX = document.querySelector('.axis-bx');
-  var axisBY = document.querySelector('.axis-by');
-
-  var pointA = document.querySelector('.point--a');
-  var pointB = document.querySelector('.point--b');
-
-  axisAX.addEventListener('input', (e) => {
-    pointA.style.left = `${e.target.value}%`;
-  });
-
-  axisAY.addEventListener('input', (e) => {
-    pointA.style.top = `${e.target.value}%`;
-  });
-
-  axisBX.addEventListener('input', (e) => {
-    pointB.style.left = `${e.target.value}%`;
-  });
-
-  axisBY.addEventListener('input', (e) => {
-    pointB.style.top = `${e.target.value}%`;
-  });
-};
+new GeoMap().init();
