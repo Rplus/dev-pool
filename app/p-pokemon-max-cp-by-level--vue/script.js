3422d6ef4a63ad225ea1fd160b57cafb109c44fb
Rplus <rplus.tw@gmail.com>
2016-11-26 06:59:15 +0800

    update to use localStorage first

--- app/p-pokemon-max-cp-by-level--vue/script.js
+++ app/p-pokemon-max-cp-by-level--vue/script.js
@@ -1,104 +1,39 @@
-/* globals fetch, Vue */
-
-const levelCpMultiplier = {
-  1: 0.094,
-  1.5: 0.135137432,
-  2: 0.16639787,
-  2.5: 0.192650919,
-  3: 0.21573247,
-  3.5: 0.236572661,
-  4: 0.25572005,
-  4.5: 0.273530381,
-  5: 0.29024988,
-  5.5: 0.306057377,
-  6: 0.3210876,
-  6.5: 0.335445036,
-  7: 0.34921268,
-  7.5: 0.362457751,
-  8: 0.37523559,
-  8.5: 0.387592406,
-  9: 0.39956728,
-  9.5: 0.411193551,
-  10: 0.42250001,
-  10.5: 0.432926419,
-  11: 0.44310755,
-  11.5: 0.453059958,
-  12: 0.46279839,
-  12.5: 0.472336083,
-  13: 0.48168495,
-  13.5: 0.4908558,
-  14: 0.49985844,
-  14.5: 0.508701765,
-  15: 0.51739395,
-  15.5: 0.525942511,
-  16: 0.53435433,
-  16.5: 0.542635767,
-  17: 0.55079269,
-  17.5: 0.558830576,
-  18: 0.56675452,
-  18.5: 0.574569153,
-  19: 0.58227891,
-  19.5: 0.589887917,
-  20: 0.59740001,
-  20.5: 0.604818814,
-  21: 0.61215729,
-  21.5: 0.619399365,
-  22: 0.62656713,
-  22.5: 0.633644533,
-  23: 0.64065295,
-  23.5: 0.647576426,
-  24: 0.65443563,
-  24.5: 0.661214806,
-  25: 0.667934,
-  25.5: 0.674577537,
-  26: 0.68116492,
-  26.5: 0.687680648,
-  27: 0.69414365,
-  27.5: 0.700538673,
-  28: 0.70688421,
-  28.5: 0.713164996,
-  29: 0.71939909,
-  29.5: 0.725571552,
-  30: 0.7317,
-  30.5: 0.734741009,
-  31: 0.73776948,
-  31.5: 0.740785574,
-  32: 0.74378943,
-  32.5: 0.746781211,
-  33: 0.74976104,
-  33.5: 0.752729087,
-  34: 0.75568551,
-  34.5: 0.758630378,
-  35: 0.76156384,
-  35.5: 0.764486065,
-  36: 0.76739717,
-  36.5: 0.770297266,
-  37: 0.7731865,
-  37.5: 0.776064962,
-  38: 0.77893275,
-  38.5: 0.781790055,
-  39: 0.78463697,
-  39.5: 0.787473578,
-  40: 0.79030001,
-  40.5: 0.7931164
-};
-
-// for dev
-if (/^\/_dist/.test(window.location.pathname)) {
-  pokeDataSrc = './baseStats.json';
-}
+/* globals localStorage, fetch, Vue */
 
 // const BEST_PROPERTY = 15;
 const MAX_LV = 40;
 const WILD_PM_MAX_LV = 30;
+const VERSION = '2016-11-22';
 
-let pokeDataSrc = 'https://cdn.rawgit.com/Rplus/358fd8f680dd2d5945c80826dfcdd599/raw/b97cfb0cccf61a119b521f43df4d449fc0eaa5f2/baseStats.json';
+let localVersion = localStorage.getItem(VERSION);
+let isOutdated = !localVersion;
+let localData = {
+  pokeData: null,
+  levelCpMultiplier: null
+};
+
+if (isOutdated) {
+  localStorage.clear();
+  localData = {};
+}
+
+let upstreamUrls = {
+  pokeData: 'https://cdn.rawgit.com/Rplus/358fd8f680dd2d5945c80826dfcdd599/raw/b97cfb0cccf61a119b521f43df4d449fc0eaa5f2/baseStats.json',
+  levelCpMultiplier: 'https://cdn.rawgit.com/Rplus/a4f16be334a5a458144c1fc425034ed3/raw/ced3970da1e6eed8b7fc4512bc2231159ad4cbde/levelCpMultiplier.json'
+};
+
+// for dev
+if (/^\/_dist/.test(window.location.pathname)) {
+  for (var name in upstreamUrls) {
+    upstreamUrls[name] = upstreamUrls[name].match(/.*\/(.+?\.json)$/)[1];
+  }
+}
 
 // source: https://github.com/vinnymac/PokeNurse/blob/v1.5.4/app/utils.js#L164-L170
 let getMaxCpForTrainerLevel = (poke, pmLv) => {
   let _iv = vm.iv;
   let maxPokemonLevel = Math.min(MAX_LV, pmLv);
-  let maxCpMultiplier = levelCpMultiplier[maxPokemonLevel];
+  let maxCpMultiplier = localData.levelCpMultiplier[maxPokemonLevel];
   let ADS = (poke.BaseAttack + _iv.attack) * Math.pow((poke.BaseDefense + _iv.defense) * (poke.BaseStamina + _iv.stamina), 0.5);
   let total = ADS * Math.pow(maxCpMultiplier, 2.0);
   return Math.floor(total / 10);
@@ -126,41 +61,41 @@ let vm = new Vue({
     pokemons: null
   },
   watch: {
-    trainerLevel: function () {
+    trainerLevel () {
       this.reCalcPmLv();
     },
-    isPmWild: function () {
+    isPmWild () {
       this.reCalcPmLv();
     },
-    pmLv: function () {
+    pmLv () {
       this.reCalcPmLv();
     }
   },
   computed: {
-    pmWidthRatio: function () {
+    pmWidthRatio () {
       return `${this.pmMaxLv * 100 / MAX_LV}%`;
     },
-    pmMaxLv: function () {
+    pmMaxLv () {
       return Math.min(this.isPmWild ? WILD_PM_MAX_LV : MAX_LV, this.trainerLevel + 1.5);
     }
   },
   methods: {
     getMaxCpForTrainerLevel,
-    reCalcPmLv: function () {
+    reCalcPmLv () {
       this.pmLv = Math.min.call(null, this.isPmWild ? WILD_PM_MAX_LV : MAX_LV, this.pmLv, this.pmMaxLv);
     },
-    getPmMaxLv: function () {
+    getPmMaxLv () {
       this.pmMaxLv = Math.min(MAX_LV, this.trainerLevel + 1.5);
     },
-    selectFamily: function (familyId) {
+    selectFamily (familyId) {
       this.filter.familyId = familyId;
     },
-    selectAll: function (all) {
+    selectAll (all) {
       [].forEach.call(document.querySelectorAll('.ctrlFilter__checkbox'), (i) => {
         i.checked = all;
       });
     },
-    sortBy: function (by = 'id') {
+    sortBy (by = 'id') {
       this.sort.dir *= (this.sort.by === by) ? -1 : 1;
       this.sort.by = by;
       let dir = this.sort.dir;
@@ -172,23 +107,51 @@ let vm = new Vue({
   }
 });
 
-fetch(pokeDataSrc)
-  .then((d) => d.json())
-  .then((d) => {
-    let pokemons = d.pokemon;
-
-    pokemons = Object.keys(pokemons).map((i) => pokemons[i]);
-
-    vm.pokemons = pokemons.map((poke, idx) => {
-      let row = ~~(idx / 7);
-      let col = idx % 7;
-
-      poke.id = idx + 1;
-      poke.familyId *= 1;
-      poke.maxcp = getMaxCpForTrainerLevel(poke, MAX_LV);
-      poke.spritePos = `${col * -65}px ${row * -65}px`;
-
-      return poke;
-    });
-    vm.$mount('#pokeMaxCP');
+let getData = (name) => {
+  return new Promise((resolve, reject) => {
+    if (isOutdated) {
+      fetch(upstreamUrls[name])
+        .then((d) => d.json())
+        .then((d) => {
+          localData[name] = d;
+          resolve(d);
+        });
+    } else {
+      let data = JSON.parse(localStorage.getItem(`${VERSION}-${name}`));
+      localData[name] = data;
+      resolve(data);
+    }
   });
+};
+
+Promise.all([
+  getData('pokeData'),
+  getData('levelCpMultiplier')
+]).then((results) => {
+  handlePokeData();
+  if (isOutdated) {
+    localStorage.setItem(VERSION, true);
+    for (var name in localData) {
+      localStorage.setItem(`${VERSION}-${name}`, JSON.stringify(localData[name]));
+    }
+  }
+});
+
+let handlePokeData = () => {
+  let pokemons = localData.pokeData.pokemon;
+
+  pokemons = Object.keys(pokemons).map((i) => pokemons[i]);
+
+  vm.pokemons = pokemons.map((poke, idx) => {
+    let row = ~~(idx / 7);
+    let col = idx % 7;
+
+    poke.id = idx + 1;
+    poke.familyId *= 1;
+    poke.maxcp = getMaxCpForTrainerLevel(poke, MAX_LV);
+    poke.spritePos = `${col * -65}px ${row * -65}px`;
+
+    return poke;
+  });
+  vm.$mount('#pokeMaxCP');
+};
